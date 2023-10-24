c9386b49e31cab1923353c20f5e2469ab8fbee02
eshoultz9 <e.shoultz9@icloud.com>
2023-10-24 12:51:52 +0000

    the pink and ze black

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 // this section initializes some variables that will be used throughout the program
 var doubleMaxSpeed = 5;
-var maxCircles = 10;
+var maxCircles = 700;
 var $board = $('#board');
 var boardWidth = $($board).width();
 var boardHeight = $($board).height();
